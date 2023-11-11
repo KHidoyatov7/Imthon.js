a33302a10c6365b838573e761cc186d8e4879cec
KHidoyatov7 <muhammadaminhidoyatov6@gmail.com>
2023-11-11 23:16:33 +0500

    I change it

--- script/signup.js
+++ script/signup.js
@@ -5,27 +5,24 @@ const $nameInput = document.querySelector("#name");
 const $avatarInput = document.querySelector("#avatar");
 const $new = document.querySelector("#new")
 $form.addEventListener("submit", signUp,);
-
 function signUp(e) {
   e.preventDefault();
   if (
-    $emailInput.value.trim().length > 0 &&
-    $passwordInput.value.trim().length > 0 &&
-    $nameInput.value.trim().length > 0 &&
-    $avatarInput.value.trim().length > 0
+    $emailInput.value.trim().length > 0 && $passwordInput.value.trim().length > 0 && $nameInput.value.trim().length > 0 && $avatarInput.value.trim().length > 0
   ) {
     fetch("https://api.escuelajs.co/api/v1/users", {
       method: "POST",
       headers: {
-        "Content-type": "application/json",
+        "Content-type": "application/json", 
       },
       body: JSON.stringify({
-        name: $nameInput.value,
-        email: $emailInput.value,
-        password: $passwordInput.value,
-        avatar: $avatarInput.value,
+        name: $nameInput.value, email: $emailInput.value, password: $passwordInput.value, avatar: $avatarInput.value,
       }),
     })
+    //       .then((response) => response.json())
+//       .then((user) => console.log(user))
+//   }
+// }
       .then((response) => response.json())
       .then((user) => {
         if (user) {
@@ -35,3 +32,4 @@ function signUp(e) {
       }); 
   }
 }
+
